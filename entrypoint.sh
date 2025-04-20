9b953c93eebaef6e0d3cc91bbbc5c974cb716992
joseaguardia <jose.a.guardia@gmail.com>
2025-04-20 15:05:15 +0200

    First functional version

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,20 +9,29 @@ if [ ! -f /root/.first_run ]; then
     echo -n
     touch /root/.first_run
     echo "kali-htb" > /etc/hostname
-    mkdir -p /root/proyectos_hack
     
     echo "🧩 Updating and installing packages..."
-    apt update
-    apt install -y apt kali-linux-headless
-    
+    export DEBIAN_FRONTEND=noninteractive
 
+    apt update
+    apt install -y \
+        git wget vim jq curl procps netcat-openbsd iproute2 telnet wfuzz \
+        iputils-ping git nmap cmseek jq gobuster whatweb seclists \
+        nikto openvpn bind9-dnsutils vim bsdmainutils exploitdb hydra medusa
+    echo -n
+    echo "🧩 Packages installed"
+    echo -n
 
     echo "cd /root/htb_machines/" >> /root/.bashrc
+    echo 'PS1='\''\[\e]0;\u@\h: \w\a\]\[\033[38;5;33m\]┌─(\[\033[38;5;148m\]\u㉿HTB\[\033[38;5;33m\])-[\[\033[1;37m\]\w\[\033[38;5;33m\]]\n\[\033[38;5;33m\]└─▣ \[\033[0m\]'\''' >> /root/.bashrc
+
 
 else
     # Connect to openvpn
     echo "Aquí conectaría a openvpn"
 fi
+
+echo "Finalizado"
 
 # Execute main command (bash by default)
 exec "$@"
