eb1699ab613f52027858ed52348f8a1a7f561b26
joseaguardia <jose.a.guardia@gmail.com>
2025-05-05 10:29:10 +0200

    feat: added icon in prompt with socat status

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,14 +27,19 @@ if [ ! -f /root/.first_run ]; then
     echo "🧩 Set up prompt, alias and variables"
     echo -n
     echo "cd /root/htb_machines/" >> /root/.bashrc
+    # Prompt
     echo 'PS1='\''\[\e]0;\u@\h: \w\a\]\[\033[38;5;33m\]┌─(\[\033[38;5;148m\]\u㉿HTB\[\033[38;5;33m\])-[\[\033[1;37m\]\w\[\033[38;5;33m\]]$(vpn_htb)\n\[\033[38;5;33m\]└─▣ \[\033[0m\]'\''' >> /root/.bashrc
     echo "clear" >> /root/.bashrc
+    # Variables
     echo 'export wordlist="/usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt"' >> /root/.bashrc
     echo 'export rockyou="/usr/share/seclists/Passwords/Leaked-Databases/rockyou.txt"' >> /root/.bashrc
     echo 'PROMPT_COMMAND="history -a; history -n"' >> /root/.bashrc
+    # Aliases
     echo 'alias vpnOff="pkill -f openvpn"' >> /root/.bashrc
-    echo 'alias vpnOn="openvpn --config /root/htb_machines/*.ovpn"' >> /root/.bashrc
+    echo 'alias vpnOn="openvpn --config /root/htb_machines/*.ovpn --daemon"' >> /root/.bashrc
+    # Functions to add icons to the prompt
     echo "vpn_htb() { ip link show tun0 &>/dev/null && echo '🔒' || echo ''; }" >> ~/.bashrc
+    echo "socat_tcp_80() { pgrep -f 'socat TCP-LISTEN:80' > /dev/null && echo '🚀' || echo ''; }" >> ~/.bashrc
 fi
 
 echo "🧩 Conecting HTB openVPN..."
