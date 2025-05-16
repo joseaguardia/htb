3d1696f4b711bc9c744359f9ff5dab5c0c807f64
joseaguardia <jose.a.guardia@gmail.com>
2025-05-16 21:07:35 +0200

    feat: added openvpn address to prompt

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ if [ ! -f /root/.first_run ]; then
     echo "cd /root/htb_machines/" >> /root/.bashrc
     
     # Prompt
-    echo 'PS1='\''\[\e]0;\u@\h: \w\a\]\[\033[38;5;33m\]┌─(\[\033[38;5;148m\]\u㉿HTB\[\033[38;5;33m\])-[\[\033[1;37m\]\w\[\033[38;5;33m\]]$(vpn_htb)$(socat_tcp_80)\n\[\033[38;5;33m\]└─▣ \[\033[0m\]'\''' >> /root/.bashrc
+    echo 'PS1='\''\[\e]0;\u@\h: \w\a\]\[\033[38;5;33m\]┌─(\[\033[38;5;148m\]\u㉿$(my_htb_ip)\[\033[38;5;33m\])-[\[\033[1;37m\]\w\[\033[38;5;33m\]]$(vpn_htb)$(socat_tcp_80)\n\[\033[38;5;33m\]└─▣ \[\033[0m\]'\''' >> /root/.bashrc
     echo "clear" >> /root/.bashrc
     
     # Variables
@@ -45,6 +45,7 @@ if [ ! -f /root/.first_run ]; then
     # Functions to add icons to the prompt
     echo "vpn_htb() { ip link show tun0 &>/dev/null && echo '🔒' || echo ''; }" >> ~/.bashrc
     echo "socat_tcp_80() { pgrep -f 'socat TCP-LISTEN:80' > /dev/null && echo '🚀' || echo ''; }" >> ~/.bashrc
+    echo "my_htb_ip() { ip -4 addr show tun0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'; }" >> ~/.bashrc
     echo "urlencode() { python3 -c \"import urllib.parse, sys; print(urllib.parse.quote(' '.join(sys.argv[1:])))\" \"\$@\"; }" >> ~/.bashrc
 
     echo -n
